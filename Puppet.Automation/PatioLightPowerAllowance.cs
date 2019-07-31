2c0afeb31af45b62319b6fed88fb4887ae5b2cb6
Cam Soper <cam.soper@outlook.com>
2019-07-31 00:16:27 -0500

    whoops

--- Puppet.Automation/PatioLightPowerAllowance.cs
+++ Puppet.Automation/PatioLightPowerAllowance.cs
@@ -1,10 +1,12 @@
 using System;
 using Puppet.Common.Automation;
+using Puppet.Common.Devices;
 using Puppet.Common.Events;
 using Puppet.Common.Services;
 
 namespace Puppet.Automation
 {
+    [TriggerDevice("Switch.PatioLight", Capability.Switch)]
     public class PatioLightPowerAllowance : PowerAllowanceBase
     {
         public PatioLightPowerAllowance(HomeAutomationPlatform hub, HubEvent evt) : base(hub, evt)
