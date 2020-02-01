963e9852a8533784eaf9140df2d2fde3c751b18c
Cam Soper <cam.soper@microsoft.com>
2020-01-31 19:29:31 -0600

    Transferring simple lighting automation

---
+++ Puppet.Automation/Lighting/Basement.cs
@@ -0,0 +1,32 @@
+﻿using Puppet.Common.Automation;
+using Puppet.Common.Devices;
+using Puppet.Common.Events;
+using Puppet.Common.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puppet.Automation.Lighting
+{
+    [TriggerDevice("Motion.Basement", Capability.Motion)]
+    public class Basement : TriggeredLightingAutomationBase
+    {
+
+        public Basement(HomeAutomationPlatform hub, HubEvent evt) : base(hub, evt)
+        {
+            DeactivationWait = TimeSpan.FromMinutes(20);
+            EnableDeactivation = true;
+        }
+
+        protected override async Task InitDevices()
+        {
+            SwitchesToActivate =
+            new List<SwitchRelay>()
+            {
+                await _hub.GetDeviceByMappedName<SwitchRelay>("Switch.ServerRoomLamp"),
+                await _hub.GetDeviceByMappedName<SwitchRelay>("Switch.BasementLamp")
+            };
+        }
+    }
+}
---
+++ Puppet.Automation/Lighting/Foyer.cs
@@ -0,0 +1,30 @@
+﻿using Puppet.Common.Automation;
+using Puppet.Common.Devices;
+using Puppet.Common.Events;
+using Puppet.Common.Services;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puppet.Automation.Lighting
+{
+    [TriggerDevice("Contact.FrontDoor", Capability.Contact)]
+    public class Foyer : TriggeredLightingAutomationBase
+    {
+        public Foyer(HomeAutomationPlatform hub, HubEvent evt) : base(hub, evt)
+        {
+            DeactivationWait = TimeSpan.FromMinutes(5);
+            EnableDeactivation = true;
+        }
+
+        protected override async Task InitDevices()
+        {
+            SwitchesToActivate =
+            new List<SwitchRelay>()
+            {
+                await _hub.GetDeviceByMappedName<SwitchRelay>("Switch.FoyerLight")
+            };
+        }
+    }
+}
