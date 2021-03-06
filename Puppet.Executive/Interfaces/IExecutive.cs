8021c32b89fd83759e693f2b5c919ccfb80600f4
Cam Soper <cam.soper@microsoft.com>
2021-03-06 13:37:14 -0600

    Refactored to use no websocket, instead uses API project

---
+++ Puppet.Executive/Interfaces/IExecutive.cs
@@ -0,0 +1,15 @@
+﻿using Puppet.Common.Events;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puppet.Executive.Interfaces
+{
+    public interface IExecutive
+    {
+        public void ProcessEvent(HubEvent evt);
+    }
+}
