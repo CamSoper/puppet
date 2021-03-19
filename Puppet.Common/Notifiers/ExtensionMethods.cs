17c6596effb45e86160ea20dc486acb246a19e66
Cam Soper <cam.soper@microsoft.com>
2021-03-18 19:01:15 -0500

    Notifiers

---
+++ Puppet.Common/Notifiers/ExtensionMethods.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puppet.Common.Notifiers
+{
+    public static class ExtensionMethods
+    {
+        public static async Task SendMessage(this IEnumerable<INotifier> notifiers, string message)
+        {
+            foreach (var n in notifiers)
+            {
+                try
+                {
+                    await n.SendNotification(message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Notify operation was unsuccessful for speaker {n} -- {ex}");
+                }
+            }
+        }
+    }
+}
---
+++ Puppet.Common/Notifiers/INotifier.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Puppet.Common.Notifiers
+{
+    public interface INotifier
+    {
+        Task SendNotification(string message);
+    }
+}
