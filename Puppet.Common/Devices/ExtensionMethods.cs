9dcca110ca89bdae91d8d7464aa144b584b02f3f
Cam Soper <cam.soper@microsoft.com>
2021-04-01 10:33:29 -0500

    .Ensure

--- Puppet.Common/Devices/ExtensionMethods.cs
+++ Puppet.Common/Devices/ExtensionMethods.cs
@@ -38,5 +38,32 @@ namespace Puppet.Common.Devices
         {
             return (device.Id == evt.DeviceId);
         }
+
+        public static async Task Ensure(this SwitchRelay switchRelay, SwitchStatus desiredState)
+        {
+            await switchRelay.RefreshState();
+            if(switchRelay.Status != desiredState && desiredState != SwitchStatus.Unknown)
+            {
+                switch (desiredState)
+                {
+                    case SwitchStatus.Off:
+                        await switchRelay.Off();
+                        break;
+
+                    case SwitchStatus.On:
+                        await switchRelay.On();
+                        break;
+                }
+            }
+        }
+        
+        public static async Task Ensure(this Fan fan, FanSpeed desiredState)
+        {
+            await fan.RefreshState();
+            if (fan.Status != desiredState && desiredState != FanSpeed.Unknown)
+            {
+                await fan.SetSpeed(desiredState);
+            }
+        }
     }
 }
